77a6087cfd942d89ee754b4382bf4710ec2c44e7
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-03-01 10:12:12 -0600

    problema de conección solucionado :D

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Ejercicios de MS Learn
 
+NOTA:
+
+1. node-redis solo soporta redis > 5.0.0
+
+2. node-redis no soporta el comando sendCommand
+
 # Ejercicios del curso de platzi
 
 [Curso de Platzi](https://platzi.com/cursos/azure-redis-cache/)
--- ms-learn/client.js
+++ ms-learn/client.js
@@ -1,25 +1,22 @@
-
-const { createClient } = require('redis');
-require('dotenv').config();
-
-const port = process.env.REDIS_PORT;
-const host = process.env.REDIS_HOST;
-console.log(host)
-const pass = process.env.REDIS_PASSWORD
-
-
+const redis = require("redis");
+require("dotenv").config();
 
 async function setInfo() {
-    const client = createClient({
-        url: `rediss://${host}:${port}`,
-        password: pass,
-    });
-    await client.connect();
-    client.on('error', (err) => console.log('Redis Client Error', err));
-    await client.ping()
-    client.set("breadWith", "Ham");
-    const res = await client.sendCommand("get", ["agua", "cate"])
-    console.log(res)
-};
+  // Connect to the Azure Cache for Redis over the TLS port using the key.
+  var cacheHostName = process.env.REDISCACHEHOSTNAME;
+  var cachePassword = process.env.REDISCACHEKEY;
+  var cacheConnection = redis.createClient({
+    // rediss for TLS
+    url: "rediss://" + cacheHostName + ":6380",
+    password: cachePassword,
+  });
+  await cacheConnection.connect();
+
+  cacheConnection.on("error", (err) => console.log("Redis Client Error", err));
+  await cacheConnection.ping();
+  cacheConnection.set("breadWith", "Ham");
+  console.log(await cacheConnection.get("breadWith"));
+  process.exit();
+}
 
 setInfo();
